dacf86a0d53cb71f8c6f7f172edefc4f5230d3c3
alexe <alexe@W10SSD>
2019-03-03 21:09:45 +0300

    Методы статического класса WebHost. Логика обработки запросов

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
@@ -15,14 +16,24 @@ namespace HelloApp
         public static void Main(string[] args)
         {
             //CreateWebHostBuilder(args).Build().Run();
-            var host = new WebHostBuilder()
-            .UseKestrel() // настраиваем веб-сервер kestrel
-            .UseContentRoot(Directory.GetCurrentDirectory()) // настраиваем корневой каталог приложения
-            .UseIISIntegration() // интеграция с IIS #endregion
-            .UseStartup<Startup>() // устанавливаем главный файл приложения
-            .Build(); // создаем хост 
 
-            host.Run(); // запускаем приложение
+            //var host = new WebHostBuilder()
+            //.UseKestrel() // настраиваем веб-сервер kestrel
+            //.UseContentRoot(Directory.GetCurrentDirectory()) // настраиваем корневой каталог приложения
+            //.UseIISIntegration() // интеграция с IIS #endregion
+            //.UseStartup<Startup>() // устанавливаем главный файл приложения
+            //.Build(); // создаем хост 
+
+            //host.Run(); // запускаем приложение
+
+            using (var host = WebHost.Start("http://localhost:8080", async context =>
+            {
+                context.Response.ContentType = "text/html; charset=utf-8";
+                await context.Response.WriteAsync("Привет мир!");
+            }))
+            {
+                host.WaitForShutdown();
+            }
         }
 
         // public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
