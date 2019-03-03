9ad8229316c1bc50d88271266dc62cb79efc65d7
alexe <alexe@W10SSD>
2019-03-03 23:45:45 +0300

    метод Use

--- HelloApp/Startup.cs
+++ HelloApp/Startup.cs
@@ -37,6 +37,19 @@ namespace HelloApp
             //}
 
             int x = 2;
+            int y = 8;
+            int z = 0;
+
+            // use позволяет после него вызывать другой контейнер
+            app.Use(async (context, next) =>
+            {
+                z = x * y;
+                await next.Invoke();
+            });
+
+
+            // То есть при вызове await next.Invoke() 
+            //обработка запроса перейдет к тому компоненту, который установлен в методе app.Run().
 
             // обработка запроса - получаем контекст запроса в ввиде объекта context
             app.Run(async (context) =>
@@ -46,8 +59,9 @@ namespace HelloApp
                 // В браузере будет выводится название приложения, которое хранится в свойстве _env.Application
                 //await context.Response.WriteAsync(_env.ApplicationName);
 
-                x = x * 2; // 2 * 2 = 4
-                await context.Response.WriteAsync($"Result: {x}"); // показывает что компоненты middleware создаются один раз при старте приложения 
+                //x = x * 2; // 2 * 2 = 4
+                //await context.Response.WriteAsync($"Result: {x}"); // показывает что компоненты middleware создаются один раз при старте приложения 
+                await context.Response.WriteAsync($"x * y = {z}");
 
             });
         }
