00f53709db5b42ca121cfe02200116d535111667
alexe <alexe@W10SSD>
2019-03-07 15:27:32 +0300

    usesql

--- HelloMvcApp/Startup.cs
+++ HelloMvcApp/Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using HelloMvcApp.Models; // пространство имен моделй 
+using Microsoft.EntityFrameworkCore; // пространство имен EntityFramework
+
 
 namespace HelloMvcApp
 {
@@ -23,15 +26,18 @@ namespace HelloMvcApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+            //services.Configure<CookiePolicyOptions>(options =>
+            //{
+            //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+            //    options.CheckConsentNeeded = context => true;
+            //    options.MinimumSameSitePolicy = SameSiteMode.None;
+            //});
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<MobileContext>(options => options.UseSqlServer(connection));
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc();
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,7 +53,7 @@ namespace HelloMvcApp
             }
 
             app.UseStaticFiles();
-            app.UseCookiePolicy();
+           // app.UseCookiePolicy();
 
             app.UseMvc(routes =>
             {
