a6120de647aa375c95c72baaf50f07b95147d467
alexe <alexe@W10SSD>
2019-03-07 15:16:15 +0300

    Add Model Class

---
+++ HelloMvcApp/Models/MobileContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloMvcApp.Models
+{
+    public class MobileContext : DbContext
+    {
+        // свойста помогают получать из бд набор данных = таблице в БД
+        public DbSet<Phone> Phones { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public MobileContext(DbContextOptions<MobileContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+    }
+}
